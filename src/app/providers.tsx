3440f79272f964a15a5fb8ee26935f9e126dd29a
ugozfb1 <ugozfb1@gmail.com>
2026-04-10 12:45:07 +0300

    Simplify providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,7 @@
 "use client";
 
-import { OnchainKitProvider } from "@coinbase/onchainkit";
-import { base } from "wagmi/chains";
 import type { ReactNode } from "react";
 
 export function Providers({ children }: { children: ReactNode }) {
-  return (
-    <OnchainKitProvider
-      apiKey={process.env.NEXT_PUBLIC_CDP_CLIENT_API_KEY}
-      chain={base}
-      config={{ appearance: { mode: "auto" } }}
-    >
-      {children}
-    </OnchainKitProvider>
-  );
+  return <>{children}</>;
 }
